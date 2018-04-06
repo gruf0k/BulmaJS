05a90390a606c3b01b0af35cf465b1a028c4068c
Thomas Erbe <vizuaalog@gmail.com>
2018-04-06 19:53:39 +0100

    Update testing config to use Karma to launch PhantomJS

--- test/CoreTest.js
+++ test/CoreTest.js
@@ -1,18 +1,14 @@
-const expect = require('chai').expect;
-
-import Bulma from '../src/core.js';
-
-describe('BulmaJS Core', () => {
-    it('create should error when trying to create an unregistered plugin', () => {
+describe('BulmaJS Core', function() {
+    it('create should error when trying to create an unregistered plugin', function() {
         expect(Bulma.create.bind(Bulma, "unregistered-plugin", {})).to.throw();
     });
 
-    it('createElement should return a div when the name of the element is div', () => {
+    it('createElement should return a div when the name of the element is div', function() {
         var myElem = Bulma.createElement('div');
         expect(myElem.nodeName).to.equal('DIV');
     });
 
-    it('createElement should return a div with a class of foo when the name of the element is div and the classes array contains foo', () => {
+    it('createElement should return a div with a class of foo when the name of the element is div and the classes array contains foo', function() {
         var myElem = Bulma.createElement('div', ['foo']);
         expect(myElem.nodeName).to.equal('DIV');
         expect(myElem.classList.contains('foo')).to.be.true;
