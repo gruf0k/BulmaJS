f05b41bf859ac11254c1727495a261eda05f69c4
Thomas Erbe <vizuaalog@gmail.com>
2018-04-20 21:35:38 +0100

    Testing tweaks

--- test/FileComponentTest.js
+++ test/FileComponentTest.js
@@ -1,41 +1,28 @@
-const expect = require('chai').expect;
-
-import Bulma from '../src/core';
-import File from '../src/plugins/file';
-
-describe('BulmaJS File Plugin', () => {
-    it('should be registered', () => {
+describe('BulmaJS File Plugin', function() {
+    it('should be registered', function() {
         expect(Bulma.hasOwnProperty('file')).to.be.true;
-    });
-
-    it('should register a change event', () => {
-        var file = makeFileHTMLWithFileName();
-
-        let events = document.createEvent('HTMLEvents');
-        events.initEvent("DOMContentLoaded", false, false);
-        document.dispatchEvent("DOMContentLoaded");
     });
 });
 
 function makeFileHTMLWithFileName() {
-    let wrapper = Bulma.createElement('div', ['file', 'has-name']);
-    let label = Bulma.createElement('label', ['file-label']);
+    var wrapper = Bulma.createElement('div', ['file', 'has-name']);
+    var label = Bulma.createElement('label', ['file-label']);
 
-    let input = Bulma.createElement('input', ['file-input']);
+    var input = Bulma.createElement('input', ['file-input']);
     input.setAttribute('type', 'file');
     input.setAttribute('name', 'resume');
 
-    let button = Bulma.createElement('span', ['file-cta']);
-    let buttonIcon = Bulma.createElement('span', ['file-icon']);
+    var button = Bulma.createElement('span', ['file-cta']);
+    var buttonIcon = Bulma.createElement('span', ['file-icon']);
     buttonIcon.appendChild(Bulma.createElement('i', ['fas', 'fa-upload']));
 
-    let buttonLabel = Bulma.createElement('span', ['file-label']);
+    var buttonLabel = Bulma.createElement('span', ['file-label']);
     buttonLabel.innerHTML = 'Choose a file?';
 
     button.appendChild(buttonIcon)
     button.appendChild(buttonLabel);
 
-    let fileName = Bulma.createElement('span', ['file-name'])
+    var fileName = Bulma.createElement('span', ['file-name'])
     fileName.innerHTML = 'No file chosen';
 
     label.appendChild(input)
